074600cfb3a344ac61c8a60cfa86daf2b69ad593
Milad <113434292+miladtorabi65@users.noreply.github.com>
2023-10-16 11:16:55 +0200

    Create 01.Introduction to Sets.py

---
+++ problem1/04.Sets/01.Introduction to Sets.py
@@ -0,0 +1,10 @@
+def average(array):
+    length = len(set(array))
+    summ = sum(set(array))
+    return summ/length
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
