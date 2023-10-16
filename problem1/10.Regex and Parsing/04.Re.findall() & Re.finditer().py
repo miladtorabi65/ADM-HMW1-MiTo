9293092ea6e41523504d5032b21b7861240d6625
Milad <113434292+miladtorabi65@users.noreply.github.com>
2023-10-16 23:34:48 +0200

    Create 04.Re.findall() & Re.finditer().py

---
+++ problem1/10.Regex and Parsing/04.Re.findall() & Re.finditer().py
@@ -0,0 +1,8 @@
+import re
+c = 'QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm'
+v = 'AEIOUaeiou'
+if __name__ == "__main__":
+    inp = input()
+    res = re.findall(r"(?<=[%s])([%s]{2,})[%s]" % (c, v, c), inp)
+    res = "\n".join(res or ['-1'])
+    print(res)
