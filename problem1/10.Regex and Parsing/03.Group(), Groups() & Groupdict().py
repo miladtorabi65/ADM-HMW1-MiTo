6d41b3cc2aa2d7afc26542d4edcab270aa7bbc5b
Milad <113434292+miladtorabi65@users.noreply.github.com>
2023-10-16 23:33:26 +0200

    Create 03.Group(), Groups() & Groupdict().py

---
+++ problem1/10.Regex and Parsing/03.Group(), Groups() & Groupdict().py
@@ -0,0 +1,8 @@
+def find_repeating_character(s):
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1] and s[i].isalnum():
+            return s[i]
+    return -1
+input_str = input()
+result = find_repeating_character(input_str)
+print(result)
