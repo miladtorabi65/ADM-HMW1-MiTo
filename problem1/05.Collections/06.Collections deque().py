bfc4f96c8fd690b51f811ae20619b84d16b01469
Milad <113434292+miladtorabi65@users.noreply.github.com>
2023-10-16 22:45:58 +0200

    Create 06.Collections deque().py

---
+++ problem1/05.Collections/06.Collections deque().py
@@ -0,0 +1,16 @@
+from collections import deque
+d = deque()
+cmds = ['append', 'pop', 'popleft', 'appendleft']
+n = int(input()) #the number of operations
+for _ in range(n):
+    x = input().split()
+    cmd = x[0]
+    if cmd == cmds[0]:
+        d.append(int(x[1]))
+    elif cmd == cmds[1]:
+        d.pop()
+    elif cmd == cmds[2]:
+        d.popleft()
+    elif cmd == cmds[3]:
+        d.appendleft(int(x[1]))
+print(*d)
